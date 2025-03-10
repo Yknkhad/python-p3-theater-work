4bf23cece14a34a56be1cd37ba896a12a0d1e7b4
Ahmed <ahmed@Ahmeds-MacBook-Pro.local>
2025-03-10 17:42:18 +0300

    complete lab

--- lib/models.py
+++ lib/models.py
@@ -1,10 +1,71 @@
-from sqlalchemy import ForeignKey, Column, Integer, String, MetaData
-from sqlalchemy.orm import relationship, backref
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey, create_engine
+from sqlalchemy.orm import relationship, sessionmaker, declarative_base
 
-convention = {
-    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
-}
-metadata = MetaData(naming_convention=convention)
+# Setting up the base class for SQLAlchemy models
+Base = declarative_base()
 
-Base = declarative_base(metadata=metadata)
+# Defining the Audition class
+class Audition(Base):
+    __tablename__ = 'auditions'
+    
+    id = Column(Integer, primary_key=True)  # Unique ID for each audition
+    actor = Column(String(50), nullable=False)  # Name of the actor
+    location = Column(String(50), nullable=False)  # Where the audition took place
+    phone = Column(String(15), nullable=False)  # Contact number
+    hired = Column(Boolean, default=False)  # Whether they got the role
+    role_id = Column(Integer, ForeignKey('roles.id'))  # Link to the Role table
+
+    role = relationship('Role', back_populates='auditions')
+
+    def call_back(self):
+        """Marks the audition as hired."""
+        self.hired = True
+
+# Defining the Role class
+class Role(Base):
+    __tablename__ = 'roles'
+
+    id = Column(Integer, primary_key=True)  # Unique ID for each role
+    character_name = Column(String)  # Name of the character
+    
+    auditions = relationship('Audition', back_populates='role')
+
+    @property
+    def actors(self):
+        """Returns a list of actor names for this role."""
+        return [audition.actor for audition in self.auditions]
+
+    @property
+    def locations(self):
+        """Returns a list of locations where auditions happened for this role."""
+        return [audition.location for audition in self.auditions]
+
+    def lead(self):
+        """Finds the first hired audition or says no one was hired."""
+        hired = [audition for audition in self.auditions if audition.hired]
+        return hired[0] if hired else 'No actor has been hired for this role'
+
+    def understudy(self):
+        """Finds the second hired audition or says no one was hired for understudy."""
+        hired = [audition for audition in self.auditions if audition.hired]
+        return hired[1] if len(hired) > 1 else 'No actor has been hired for understudy for this role'
+
+
+# Setting up the database
+engine = create_engine('sqlite:///theater.db')  
+Base.metadata.create_all(engine)  
+Session = sessionmaker(bind=engine)  
+session = Session()  
+
+# Creating a role
+role = Role(character_name='Hamlet')  
+
+# Creating auditions
+audition1 = Audition(actor='Thomus Maina', location='New York', phone='1723466238', role=role)
+audition2 = Audition(actor='Sharon Talia', location='Los Angeles', phone='9876543210', role=role)  
+audition3 = Audition(actor='Ruth Ashley', location='Chicago', phone='6756348976', role=role)  
+
+# Adding everything to the database
+session.add(role)  
+session.add_all([audition1, audition2, audition3])  
+session.commit()  
